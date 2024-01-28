afd9399eb00c59132556242a886c7fae575bc793
Josh Hackney <jhaxllc@gmail.com>
2024-01-27 22:51:16 -0500

    updated make files to include new reverse function - unfortunately something is borked and list essentially empties now

---
+++ Linked_List/cpp/src/reverse.cpp
@@ -0,0 +1,33 @@
+#include "../include/deleteN.h"
+#include "../include/nodeStruct.h"
+
+#include <iostream>
+
+void reverseI(struct Node *head)
+/* reverse linked list values using iterative approach */
+{
+    /* STEP 1 */
+    struct Node *cur, *prev, *next; // create nodes to hold data for current, previous, and next nodes
+
+    /* STEP 2 */
+    cur = head; // go to first node in the list, pointed to by head
+
+    /* STEP 3 */
+    prev = NULL; // as we're reversing the list, the last node will point to null
+
+    /* STEP 4 */
+    while (cur != NULL) // Loop over list until current is null
+                        // prev will be the old last node/new first node at end of loop
+    {
+        /* STEP 4A */
+        next = cur->next; // first step is storing the next node before we break the link to it
+        /* STEP 4B */
+        cur->next = prev; // as mentioned current will now point to previous node instead of next node
+        /* STEP 4C */
+        prev = cur; // move previous up one node
+        /* STEP 4D */
+        cur = next; // move current to the next node, using
+    }
+    /* STEP 5*/
+    head = prev; // list is now reversed, so we point head to the old last/new first node
+}
